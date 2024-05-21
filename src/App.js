c6e366bbad9d7d9e8d461d162c67e85c465630f5
mandalilevy <mandalilevy@gmail.com>
2024-05-21 05:36:33 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Student from './components/Student';
+function App() {
+  return (
+    <div className="App">
+     <Student/>
+    </div>
+  );
+}
+
+export default App;
